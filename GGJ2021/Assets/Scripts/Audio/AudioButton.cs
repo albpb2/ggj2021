17a25a1f0209b2eb669e34138d3aba321c2b8feb
albpb2 <albpb2@gmail.com>
2021-01-30 22:10:27 +0000

    Create AudioButton script

---
+++ GGJ2021/Assets/Scripts/Audio/AudioButton.cs
@@ -0,0 +1,20 @@
+﻿using FMODUnity;
+using UnityEngine;
+
+namespace Audio
+{
+    public class AudioButton : MonoBehaviour
+    {
+        private StudioEventEmitter _studioEventEmitter;
+        
+        private bool _playing;
+
+        public void PlayOrStop()
+        {
+            if (_playing)
+                _studioEventEmitter.Stop();
+            else
+                _studioEventEmitter.Play();
+        }
+    }
+}
