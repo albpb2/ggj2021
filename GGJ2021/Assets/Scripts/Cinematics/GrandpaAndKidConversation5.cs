78e14c3f9b8a9215c80770f127dcb7a55aaf2db4
Beatriz Diaz <beatriz.diaz.tobarra@gmail.com>
2021-01-31 03:37:36 +0100

    Dialoges Bea

---
+++ GGJ2021/Assets/Scripts/Cinematics/GrandpaAndKidConversation5.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using Cinematics;
+using UnityEngine;
+
+public class GrandpaAndKidConversation5: MonoBehaviour
+{
+    
+
+    [Header("Dialog lines")]
+    [SerializeField] private DialogText _grandpaDialog1_fondo_negro;  
+    [SerializeField] private DialogText _kidDialog1_con_abuelo;
+    [SerializeField] private DialogText _grandpaDialog2;        
+    [SerializeField] private DialogText _kidDialog2;
+    [SerializeField] private DialogText _grandpaDialog3;
+    [SerializeField] private DialogText _kidDialog3;
+    [SerializeField] private DialogText _grandpaDialog4;        
+    [SerializeField] private DialogText _kidDialog4;
+    [SerializeField] private DialogText _grandpaDialog5; 
+    [SerializeField] private DialogText _kidDialog5;
+    [SerializeField] private DialogText _grandpaDialog6; 
+    [SerializeField] private DialogText _kidDialog6;
+    [SerializeField] private DialogText _grandpaDialog7; 
+    [SerializeField] private DialogText _kidDialog7;
+    [SerializeField] private DialogText _grandpaDialog8; 
+	[SerializeField] private DialogText _kidDialog8;
+}
+
