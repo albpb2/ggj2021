f6763e5f6371333200947077e7ab6cbd4983d179
albpb2 <albpb2@gmail.com>
2021-01-31 12:28:52 +0000

    Add door open sound

--- GGJ2021/Assets/Scripts/InteractableElements/Door.cs
+++ GGJ2021/Assets/Scripts/InteractableElements/Door.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using FMODUnity;
+using UnityEngine;
 
 namespace InteractableElements
 {
@@ -6,16 +7,21 @@ namespace InteractableElements
     {
         private Collider2D _collider;
         private Animator _animator;
+        private StudioEventEmitter _studioEventEmitter;
 
         private void Awake()
         {
             _collider = GetComponent<Collider2D>();
             _animator = GetComponent<Animator>();
+            _studioEventEmitter = GetComponent<StudioEventEmitter>();
         }
 
         public void Open()
         {
+            _studioEventEmitter.Play();
+
             _collider.enabled = false;
+
             const string openAnimationTrigger = "Open";
             _animator.SetTrigger(openAnimationTrigger);
         }
