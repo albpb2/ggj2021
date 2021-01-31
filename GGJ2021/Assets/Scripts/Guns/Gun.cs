fde54a782cc1f3602699d63f604fded19b9816ee
albpb2 <albpb2@gmail.com>
2021-01-31 12:17:53 +0000

    Add event emitter to blaster

--- GGJ2021/Assets/Scripts/Guns/Gun.cs
+++ GGJ2021/Assets/Scripts/Guns/Gun.cs
@@ -1,16 +1,28 @@
-﻿using UnityEngine;
+﻿using System;
+using FMODUnity;
+using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
     [SerializeField] private ProjectilePool _projectilePool;
     [SerializeField] private float _reloadTime;
 
+    private StudioEventEmitter _studioEventEmitter;
+
     private float _lastShootTime;
+
+    private void Awake()
+    {
+        _studioEventEmitter = GetComponent<StudioEventEmitter>();
+    }
 
     public void Shoot(Transform shootPoint, Vector2 direction)
     {
         if (!CanShoot())
             return;
+        
+        if (_studioEventEmitter != null)
+            _studioEventEmitter.Play();
         
         var projectile = _projectilePool.GetNextItem();
         projectile.Shoot(shootPoint.position, direction);
