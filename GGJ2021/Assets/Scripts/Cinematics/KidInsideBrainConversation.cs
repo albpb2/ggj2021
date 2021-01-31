2781e72f2fb4871cbbcc4d9f7d01bf49f91a60e8
albpb2 <albpb2@gmail.com>
2021-01-31 15:43:48 +0000

    Start work on kid inside brain conversation

---
+++ GGJ2021/Assets/Scripts/Cinematics/KidInsideBrainConversation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using Cinematics;
+using UnityEngine;
+
+public class KidInsideBrainConversation : Cinematic
+{
+    [Header("Characters")]
+    [SerializeField] private CinematicCharacter _kid;
+    [SerializeField] private CinematicCharacter _grandpa;
+
+    [Header("Dialog boxes")] 
+    [SerializeField] private DialogBox _kidDialogBox;
+
+    [Header("Dialog lines")]
+    [SerializeField] private DialogText _kidDialog1;
+    [SerializeField] private DialogText _kidDialog2;
+    [SerializeField] private DialogText _kidDialog3;
+    [SerializeField] private DialogText _kidDialog4;
+
+    private bool _isFinished;
+
+    public override void Play()
+    {
+        StartCoroutine(PlayCinematic());
+    }
+
+    public override bool IsFinished() => _isFinished;
+
+    private IEnumerator PlayCinematic()
+    {
+        yield return new Speak(_kidDialogBox, _kidDialog1);
+        yield return new Speak(_kidDialogBox, _kidDialog2);
+        yield return new Speak(_kidDialogBox, _kidDialog3);
+        yield return new Speak(_kidDialogBox, _kidDialog4);
+        _isFinished = true;
+    }
+}
--- GGJ2021/Assets/Scripts/State/GlobalGameState.cs
+++ GGJ2021/Assets/Scripts/State/GlobalGameState.cs
@@ -1,4 +1,6 @@
 ﻿public class GlobalGameState : Singleton<GlobalGameState>
 {
     public int GrandpaHouseCinematicIndex { get; set; }
+    
+    public bool WarSceneCinematicPlayed { get; set; }
 }
