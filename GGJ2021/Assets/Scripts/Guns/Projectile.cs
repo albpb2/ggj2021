60bb302c4a0f1a8279c7a66b12211cce107f0fed
albpb2 <albpb2@gmail.com>
2021-01-31 03:36:39 +0000

    Add turret script to first turret

--- GGJ2021/Assets/Scripts/Guns/Projectile.cs
+++ GGJ2021/Assets/Scripts/Guns/Projectile.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private int _damage;
+    
+    private PlayerController _playerController;
     
     private Rigidbody2D _rigidbody;
     
     private void Awake()
     {
+        _playerController = FindObjectOfType<PlayerController>();
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.CompareTag(Tags.Player))
+        {
+            _playerController.Hurt(_damage);
+            Disable();
+            return;
+        }
+        
         if (!other.isTrigger)
             Disable();
     }
